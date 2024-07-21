d5dcabc13cb4a548f77f97236e4514daea97bc9b
Rodrigo G Lima <rodgarcialima@gmail.com>
2024-07-21 19:57:12 -0400

    adding tailwind-variants

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,40 @@
 import React, { ComponentProps } from "react";
+import { tv, type VariantProps } from 'tailwind-variants';
 
-export type ButtonProps = ComponentProps<'button'> & {}
+const buttonStyles = tv({
+    base: 'px-4 py-1.5 rounded-full hover:opacity-80',
+    variants: {
+      color: {
+        primary: 'bg-blue-500 text-white',
+        neutral: 'bg-zinc-500 text-black dark:text-white'
+      },
+      flat: {
+        true: 'bg-transparent'
+      }
+    },
+    defaultVariants: {
+      color: 'primary'
+    },
+    compoundVariants: [
+      {
+        color: 'primary',
+        flat: true,
+        class: 'bg-blue-500/40'
+      },
+      {
+        color: 'neutral',
+        flat: true,
+        class: 'bg-zinc-500/20'
+      }
+    ]
+});
+
+type ButtonVariants = VariantProps<typeof buttonStyles>;
+
+export type ButtonProps = ComponentProps<'button'> & ButtonVariants;
 
 const Button = (props: ButtonProps) => {
-    return <button type='button' data-testid='button' className="text-red-400 text-lg" {...props} />;
+    return <button className={buttonStyles(props)} data-testid='button' />;
 };
 
 export default Button;
